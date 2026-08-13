f950c617ac84ae07ead5e6861ce25865d9d59ef9
Shrinivas <shrinivasrj734@gmail.com>
2026-08-13 10:38:05 +0530

    Phase 0 of the project

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    } catch(err){
+        console.error('MongoDB failed to connect', err.message);
+        process.exit(1);
+
+    }
+};
+
+module.exports = connectDB;
