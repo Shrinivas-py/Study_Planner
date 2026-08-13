4af798d5e92102617b636f286dab1c9ccc3bc476
Shrinivas <shrinivasrj734@gmail.com>
2026-08-13 11:53:34 +0530

    phase 3 where i worked on auths, middlewares

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const app = express();
 connectDB();
 app.use(cors());
 app.use(express.json());
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 
 app.get('/api/health', (req,res)=>{
     res.json({status : 'ok'});
